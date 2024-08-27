f120736e177f336437ccc5a6703beb19de74ab8f
Millie <saravymilena@gmail.com>
2024-08-27 18:50:07 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import visao.ClasseProdutoVisao;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ClasseProdutoVisao visao = new ClasseProdutoVisao();
+        visao.Exibir();
+        System.out.println("Hello World");
+    }
+}
---
+++ src/visao/ClasseProdutoVisao.java
@@ -0,0 +1,24 @@
+package visao;
+
+import java.time.LocalDate;
+
+import dominio.ClasseProduto;
+
+public class ClasseProdutoVisao {
+    public void Exibir(){
+        ClasseProduto cp1 = new ClasseProduto();
+        cp1.setCodigo(1);
+        cp1.setDescricao("Carnes");
+        cp1.setDataDeInclusao(LocalDate.now());
+        System.out.println("Classe de produto: ");
+        System.out.println("Código: " + cp1.getCodigo());
+        System.out.println("Descrição: " + cp1.getDescricao());
+        System.out.println("Data de inclusão: " + cp1.getDataDeInclusao());
+        
+        ClasseProduto cp2 = new ClasseProduto(2, "Bebidas", LocalDate.now());
+        System.out.println("Classe de produto: ");
+        System.out.println("Código: " + cp2.getCodigo());
+        System.out.println("Descrição: " + cp2.getDescricao());
+        System.out.println("Data de inclusão: " + cp2.getDataDeInclusao());
+    }
+}
